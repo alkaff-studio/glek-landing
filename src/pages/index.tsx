271d09646185671d37c25125f1da6153ee6df835
Ahmad Faris <isfawwaz@gmail.com>
2020-12-18 17:35:44 +0700

    Setup section - section about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import { PageProps, Link, graphql } from "gatsby"
 import Layout from '../components/layout'
 import SEO from "../components/seo"
 import SectionJumbotron from "../sections/jumbotron"
+import SectionAbout from "../sections/about"
 
 const IndexPage: React.FC = () => (
   <Layout invertButton={true}>
     <SEO title="Halaman Utama" />
     <SectionJumbotron />
+    <SectionAbout />
   </Layout>
 )
 
