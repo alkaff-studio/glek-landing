24a24ec2083941aa374e8371e9712eb27a776e15
Ahmad Faris <isfawwaz@gmail.com>
2020-12-18 22:16:09 +0700

    Responsive section - jumbotron

--- src/sections/jumbotron.tsx
+++ src/sections/jumbotron.tsx
@@ -14,13 +14,13 @@ const SectionJumbotron = () => (
         <div className="jumbotron-content">
             <div className="container-fluid px-0">
                 <div className="row no-gutters align-items-stretch justify-content-start">
-                    <div className="col-12 col-md-6">
+                    <div className="col-12 col-md-6 order-last order-md-first">
                         <div className="jumbotron-container ml-md-5 pl-md-5">
                             <h1 className="typo-display-md-bold typo-gray-body">Hausnya Hilang Berkahnya Datang</h1>
                             <p className="typo-text-md typo-gray-label">Berniaga Dengan Allah. 51% Keuntungan untuk Sedekah</p>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6">
+                    <div className="col-12 col-md-6 order-first order-md-last">
                         <div className="jumbotron-splash"></div>
                     </div>
                 </div>
