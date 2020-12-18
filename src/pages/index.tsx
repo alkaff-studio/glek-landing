52db941605bfdc8d0dda5aab15b871e646eae357
Ahmad Faris <isfawwaz@gmail.com>
2020-12-18 17:50:06 +0700

    Setup section - vision mission

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import Layout from '../components/layout'
 import SEO from "../components/seo"
 import SectionJumbotron from "../sections/jumbotron"
 import SectionAbout from "../sections/about"
+import SectionVisionMision from "../sections/vision"
 
 const IndexPage: React.FC = () => (
   <Layout invertButton={true}>
     <SEO title="Halaman Utama" />
     <SectionJumbotron />
     <SectionAbout />
+    <SectionVisionMision />
   </Layout>
 )
 
---
+++ src/sections/vision.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const SectionVisionMision = () => (
+    <section id="section-vision-mission" className="section">
+        <div className="container-fluid">
+            <div className="row no-gutters">
+                <div className="col-12 col-md-6">
+                    <div className="section-content ml-md-5 pl-md-5">
+                        <div className="info-section">
+                            <h6 className="typo-display-sm-bold typo-gray-body">Visi</h6>
+                            <p className="typo-text-md typo-gray-label">Perusahaan waralaba omni-channel berbasis syariah dengan kontribusi sedekah terbesar di Indonesia.</p>
+                        </div>
+                        <div className="info-section">
+                            <h6 className="typo-display-sm-bold typo-gray-body">Misi</h6>
+                            <ol className="list-unstyled list-check">
+                                <li>
+                                    <span>Mewujudkan SDM yang berakhlak mulia dan bermanfaat</span>
+                                </li>
+                                <li>
+                                    <span>Meningkatkan kinerja perusahaan yang berorientasi pada ekonomi umat</span>
+                                </li>
+                                <li>
+                                    <span>Meningkatkan sarana dan prasarana dalam peningkatan ketaqwaan dan kontribusi sedekah</span>
+                                </li>
+                                <li>
+                                    <span>Membangun sinergitas tanpa batas yang harmonis</span>
+                                </li>
+                                <li>
+                                    <span>Menjalin kerjasama yang harmonis dan dinamis dengan mitra terkait</span>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6">
+                    <div className="section-img"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+)
+
+export default SectionVisionMision
